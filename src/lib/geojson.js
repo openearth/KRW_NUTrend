9ac371a405e6c93b99960e8978932e91da81997a
IoannaMi <ioannamicha@hotmail.com>
2021-09-13 09:40:55 +0200

    Build geojson-feature collection from data

---
+++ src/lib/geojson.js
@@ -0,0 +1,17 @@
+export const geojson = (timeseries) => ({
+         type: "FeatureCollection",
+         features: timeseries.map(waterbody=> ({
+           type: "Feature",
+           properties: {
+             locationId: waterbody.header.locationId,
+             value: waterbody.events[0].value,
+           },
+           geometry: {
+             type: "Point",
+             coordinates: [
+               waterbody.header.lon,
+               waterbody.header.lat,
+             ],
+           },
+         })),
+       })
