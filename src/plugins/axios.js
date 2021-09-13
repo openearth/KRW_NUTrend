a3a763203bc4c4fde2ee4addee2628e456f64cc2
IoannaMi <ioannamicha@hotmail.com>
2021-09-13 09:41:40 +0200

    Update axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -5,15 +5,11 @@ import axios from 'axios'
 
 // request config: https://github.com/axios/axios#request-config
 const AXIOS_CONFIG = {
-  auth: {
-    username: process.env.VUE_APP_API_USERNAME,
-    password: process.env.VUE_APP_API_PASSWORD,
-  },
+  baseURL: process.env.VUE_APP_API_ENDPOINT,
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
   },
 }
-
 const instance = axios.create(AXIOS_CONFIG)
 
 Vue.prototype.$axios = instance
