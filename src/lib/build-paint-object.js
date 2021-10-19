00485bb7a09537a3f543a7a185c5c185cc225ffa
IoannaMi <ioannamicha@hotmail.com>
2021-10-19 11:48:00 +0200

    Add circle radius

--- src/lib/build-paint-object.js
+++ src/lib/build-paint-object.js
@@ -1,6 +1,9 @@
 export default (circlesColors) => {
   return {
     'circle-radius': 6,
+    'circle-stroke-opacity': 1,
+    'circle-stroke-color': '#000000',
+    'circle-stroke-width': 1,
     'circle-color': circlesColors,
   }
 }
