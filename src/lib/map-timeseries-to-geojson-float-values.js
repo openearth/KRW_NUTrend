1d5164fb31cbc705ef82ed905ac749da7a7d7754
IoannaMi <ioannamicha@hotmail.com>
2021-10-19 11:49:53 +0200

    rename to float values
    * It is used also from other layers that have range values instead of match cases

--- src/lib/map-timeseries-to-geojson-float-values.js
+++ src/lib/map-timeseries-to-geojson-float-values.js
@@ -2,20 +2,19 @@ export default (data) => {
   if (!data?.timeSeries.length) {
     return []
   }
-
   const features = data.timeSeries.map(({ events, header }) => ({
     type: 'Feature',
     properties: {
       locationId: header.locationId,
-      value: parseFloat(events[0].value),
-      value2: events[events.length - 1].value,
+      value: events ? parseFloat(events[0].value) : 'null',
+      value2: events ? events[events.length - 1].value : 'null',
     },
     geometry: {
       type: 'Point',
       coordinates: [ header.lon, header.lat ],
     },
   }))
-
+ 
   return {
     type: 'FeatureCollection',
     features,
