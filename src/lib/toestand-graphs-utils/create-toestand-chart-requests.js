1c7a030daa12d5c12c6248342b412978e47eee26
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2025-02-05 18:57:57 +0100

    patch request for graph visualisation

--- src/lib/toestand-graphs-utils/create-toestand-chart-requests.js
+++ src/lib/toestand-graphs-utils/create-toestand-chart-requests.js
@@ -14,10 +14,23 @@ export default((charts, time=null, locations=null) => {
       documentFormat:'PI_JSON',
     }
     if (time) {
-      params = {
-        ...params,
-        startTime: time,
-        endTime: time,
+      //NOTE: When making a few request with a startTime and endTime that do not match exactly what is available we we two values instead of one, messing up the visualisation. It is now patched by requesting data from 31-12-23:00:00 of the previous year to be plotted instead of from 01-01-00:00:00 since that gives two results.
+      
+      // Trim spaces and ensure valid date format
+      const sanitizedTime = time.replace(/\s+/g, '')
+      const date = new Date(sanitizedTime)
+      
+      if (!isNaN(date.getTime())) {
+        date.setHours(date.getHours() - 1)
+        const adjustedTime = date.toISOString()
+        
+        params = {
+          ...params,
+          startTime: adjustedTime,
+          endTime: adjustedTime,
+        }
+      } else {
+        console.error('Invalid time value after sanitization:', sanitizedTime)
       }
     }
     if (locations) {
